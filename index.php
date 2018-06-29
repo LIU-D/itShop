a56416990ec92d96f65e5f9a83d5a66eee950e77
LIU-D <943445214@qq.com>
2018-06-29 16:55:43 +0800

    init
    项目开始前的准备

--- index.php
+++ index.php
@@ -14,6 +14,9 @@
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
+//浏览器响应类型设定
+header('Content-Type:text/html;charset=utf-8');
+
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 //true调试模式
 //false生产模式
--- namedemo/b/y.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace b;
-class Liu{
-    function L(){
-        echo "Y_LIU";
-    }
-}
-
